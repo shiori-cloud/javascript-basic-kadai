87f82ac36206293db81dca0ee625c04d6796bdda
shiori-cloud <kanyuantian69@gmail.com>
2026-01-28 21:34:10 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const moonLanding = new Date();
+
+let year = moonLanding.getFullYear();
+let month = moonLanding.getMonth() + 1;
+let date = moonLanding.getDate();
+
+console.log(year + '年' + month + '月' + date + '日');
